1b9984947cf6ba2df7cc3634c3c85e270a26294a
Murat Byazrov <murat.byazrov@yandex.ru>
2020-07-08 08:13:44 +0300

    Полностью настроил вебпак, включая работу с css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
 // утилита, для понимания относит пути вебпаком
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); // Подключили к проекту плагин
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // для работы с html
+const WebpackMd5Hash = require('webpack-md5-hash'); // хеширует и сравнивает хеш
 
 module.exports = {
   // точка входа
@@ -8,6 +11,30 @@ module.exports = {
   output: {
     // ссылка на текущую папку и относитльный путь к точке выхода(относительно текущей директории)
     path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
+    filename: '[name].[chunkhash].js',
   },
+  module: {
+    rules: [{ // тут описываются правила
+      test: /\.js$/, // регулярное выражение, которое ищет все js файлы
+      use: { loader: 'babel-loader' }, // весь JS обрабатывается пакетом babel-loader
+      exclude: /node_modules/, // исключает папку node_modules
+    },
+    {
+      test: /\.css$/, // применять это правило только к CSS-файлам
+      use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'], // к этим файлам нужно применить пакеты
+    },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      // Означает, что:
+      inject: false, // стили НЕ нужно прописывать внутри тегов
+      template: './src/index.html', // откуда брать образец для сравнения с текущим видом проекта
+      filename: 'index.html', // имя выходного файла, то есть того, что окажется в папке dist после сборки
+    }),
+    new WebpackMd5Hash(),
+  ],
 };
