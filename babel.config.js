90b0c7c4b9ea8d20c7abe0c3464cd0ae1a55abe3
Murat Byazrov <murat.byazrov@yandex.ru>
2020-07-08 08:53:27 +0300

    Настроил бабель, чтобы он поддерживал нужные браузеры Добавил два плагина для оптимизации изображений

--- babel.config.js
+++ babel.config.js
@@ -2,13 +2,13 @@ const presets = [
   [
     '@babel/env',
     {
-      targets: { // указать цели, для полифилов
-        edge: '17',
-        ie: '11',
-        firefox: '50',
-        chrome: '64',
-        safari: '11.1',
-      },
+      targets: [
+        'last 2 Chrome versions',
+        'last 2 ChromeAndroid versions',
+        'last 2 Firefox versions',
+        'last 2 Safari versions',
+        'Edge >= 15',
+      ],
       useBuiltIns: 'usage', // эта настройка babel-polyfill, если стоит значение usage, то будут подставлятся полифилы для версий браузеров которые указали ниже.
       corejs: '3.4.1', // явно проставить версию corejs
     },
