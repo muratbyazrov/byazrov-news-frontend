723fea03c0209b83a4e3a57c3ee2eee4ccb124fe
Murat Byazrov <murat.byazrov@yandex.ru>
2020-08-19 12:09:33 +0300

    Убрал у метода гетньюз метод - все равно работает

--- src/js/api/NewsApi.js
+++ src/js/api/NewsApi.js
@@ -5,8 +5,6 @@ export default class NewsApi {
 
   // eslint-disable-next-line class-methods-use-this
   getNews(keyWord, actualDate) {
-    return fetch(`http://newsapi.org/v2/everything?q=${keyWord}&from=${actualDate}&sortBy=popularity&apiKey=0809fef65efa4e06b9681496b0a3fa07`, {
-      method: 'GET',
-    });
+    return fetch(`http://newsapi.org/v2/everything?q=${keyWord}&from=${actualDate}&sortBy=popularity&apiKey=0809fef65efa4e06b9681496b0a3fa07`);
   }
 }
